3d331950b0232348248b615d47d5b37f8d849ac8
abhiram-98498 <148839109+abhiram-98498@users.noreply.github.com>
2025-12-18 18:33:11 +0530

    Add RealDictCursor to database connection

--- backend/db.py
+++ backend/db.py
@@ -1,18 +1,19 @@
 import psycopg2
 import os
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection():
     try:
-        connection = psycopg2.connect(
+        return psycopg2.connect(
             user=os.getenv("user"),
             password=os.getenv("password"),
             host=os.getenv("host"),
             port=os.getenv("port"),
-            dbname=os.getenv("dbname")
+            dbname=os.getenv("dbname"),
+            cursor_factory=RealDictCursor
         )
-        return connection
     except Exception as e:
         raise RuntimeError(f"Database connection failed: {e}")
